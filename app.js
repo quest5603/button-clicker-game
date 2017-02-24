44cf7f96825d8a2010eb07a19557913ff4faf694
quest5603 <quest5602@gmail.com>
2017-02-23 15:30:27 -1000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+console.log("jello")
+alert("hi")
+
+function buttonamount(){
+
+
+	document.getElementById("hello").innerHTML = "hello";
+}
+
+var clicks =0;
+function buttonamount() {
+	clicks+=77;
+document.getElementById("yay").innerHTML = clicks;
+}
